1f0e5e08a945be0dac56d4bb3e6112c40a37173a
Ali Bölücü <48453081+Ali-Bolucu@users.noreply.github.com>
2021-02-28 13:10:57 +0300

    name change and Market button
    Names and picture have changed and now next step is market history and buy/sell orders

--- popup.js
+++ popup.js
@@ -5,7 +5,22 @@ document.addEventListener(
     checkPageButton.addEventListener(
       "click",
       function () {
-        window.open("new_page.html", "_blank");
+        window.open("Inventory.html", "_blank");
+      },
+      false
+    );
+  },
+  false
+);
+
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    var checkPageButton = document.getElementById("market");
+    checkPageButton.addEventListener(
+      "click",
+      function () {
+        window.open("Market_history.html", "_blank");
       },
       false
     );
